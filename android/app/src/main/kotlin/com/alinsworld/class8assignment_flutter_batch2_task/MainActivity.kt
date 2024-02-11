64d80db0f05cea90fdb4eaf94bf68e761365405a
rezwansaki <rezwansaki@gmail.com>
2024-02-11 19:28:33 +0600

    Init

---
+++ android/app/src/main/kotlin/com/alinsworld/class8assignment_flutter_batch2_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alinsworld.class8assignment_flutter_batch2_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('UI Design'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 8),
+              TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(42.0),
+                    borderSide: const BorderSide(width: 3, color: Colors.green),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 8),
+              const TextField(
+                decoration: InputDecoration(
+                  errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(width: 3, color: Colors.black),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              const TextField(
+                decoration: InputDecoration(
+                  errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(width: 5, color: Colors.green),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              const TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(width: 5, color: Colors.greenAccent),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
